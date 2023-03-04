d584f9e26f3ad3ac8bc46c769e6c091273d8a472
thompson0012 <51963680+thompson0012@users.noreply.github.com>
2023-03-05 00:08:07 +0800

    Update main.py

--- chatgpt_cli/main.py
+++ chatgpt_cli/main.py
@@ -1,8 +1,4 @@
-# User: Can you write it in the library combination of Typer, openai
 
-# Sage: Certainly! Here's an example [Python script](poe://www.poe.com/_api/key_phrase?phrase=Python%20script&prompt=Tell%20me%20more%20about%20Python%20script.) that uses the [Typer library](poe://www.poe.com/_api/key_phrase?phrase=Typer%20library&prompt=Tell%20me%20more%20about%20Typer%20library.) and the OpenAI GPT-3 API to create a command-line interface for a chatbot that can generate text based on user input:
-
-# ```
 import openai
 import os
 import typer
@@ -46,20 +42,3 @@ def chat(model: str = typer.Argument("gpt-3.5-turbo")):
         prompt.append(response)
         # Print the generated response
         typer.echo(f"ChatGPT: {response['content']}")
-
-# Run the Typer app
-# if __name__ == "__main__":
-#     app()
-# ```
-#
-# This script uses the Typer library to define a command-line interface for the chatbot, and the [OpenAI API](poe://www.poe.com/_api/key_phrase?phrase=OpenAI%20API&prompt=Tell%20me%20more%20about%20OpenAI%20API.) to generate text based on user input. The `chat_with_gpt` function interacts with the GPT-3 model to generate text based on user input, and the `chat` command starts the chatbot and prompts the user for input.
-#
-# To run the script, save it to a file with a `.py` extension and run it from the command line using the `python` command. For example, if you saved the script to a file named `chatbot.py`, you could run it using the following command:
-#
-# ```
-# python chatbot.py chat
-# ```
-#
-# This would start the chatbot and prompt the user for input. The user can then enter text and the chatbot will generate a response based on the input, using the [OpenAI GPT](poe://www.poe.com/_api/key_phrase?phrase=OpenAI%20GPT&prompt=Tell%20me%20more%20about%20OpenAI%20GPT.)-3 language model.
-#
-# I hope this example helps you get started with using the Typer library and the OpenAI API to create a command-line chatbot that generates text based on user input! Let me know if you have any further questions or need additional assistance.
